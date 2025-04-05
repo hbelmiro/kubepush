b02408aaa72c16b55fccbe7d844f826630107c23
Helber Belmiro <helber.belmiro@gmail.com>
2025-04-05 11:32:49 -0300

    Used a temp file
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"log"
+	"os"
 	"os/exec"
 )
 
@@ -17,7 +18,22 @@ func main() {
 			clusterName, _ := cmd.Flags().GetString("cluster-name")
 
 			log.Println("Saving image file...")
-			c := exec.Command("sh", "-c", "podman save -o ~/Downloads/image.tar "+image)
+
+			tmpFile, err := os.CreateTemp("", "podman-image-*.tar")
+			if err != nil {
+				return fmt.Errorf("error creating temp file: %d", err)
+			}
+			defer func(name string) {
+				log.Println("Removing image file...")
+				err := os.Remove(name)
+				if err == nil {
+					log.Println("Image file removed successfully")
+				} else {
+					log.Fatalf("error removing temp file: %d", err)
+				}
+			}(tmpFile.Name())
+
+			c := exec.Command("sh", "-c", "podman save -o "+tmpFile.Name()+" "+image)
 			output, err := c.CombinedOutput()
 			if err != nil {
 				if output != nil {
@@ -28,7 +44,7 @@ func main() {
 			}
 
 			log.Println("Image file saved. Loading image into the cluster...")
-			c = exec.Command("sh", "-c", "kind load image-archive --name "+clusterName+" ~/Downloads/image.tar")
+			c = exec.Command("sh", "-c", "kind load image-archive --name "+clusterName+" "+tmpFile.Name())
 			output, err = c.CombinedOutput()
 			if err != nil {
 				if output != nil {
